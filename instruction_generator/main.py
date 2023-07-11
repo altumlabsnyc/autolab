3d93e119ce9f2b669d519b54def40928e6fabb7f
isabellaqian <izzy@altumlabs.co>
2023-07-10 21:27:02 -0400

    added compatability for mac/windows, finetune gpt

--- instruction_generator/main.py
+++ instruction_generator/main.py
@@ -1,22 +1,32 @@
 from gpt_transcript import TranscriptConversion
 import os
+import platform
 from dotenv import load_dotenv
 
 if __name__ == "__main__":
     cwd = os.getcwd()
-    # print(cwd)
 
     # set input and output directories (input dir must exist before running)
-    input_dir = f"{cwd}\\instruction_generator\\data\\transcript_time.txt"
-    output_dir = f"{cwd}\\instruction_generator\\data\\outputs\instruction_set.txt"
+    system = platform.system()
+    sep = None
+    if system == "Darwin":  # mac
+        sep = "/"
+    elif system == "Windows":  # windows
+        sep = "\\"
+    else:
+        sep = "/"
+    input_dir = f"{cwd}{sep}data{sep}transcript_time.txt"
+    output_dir = f"{cwd}{sep}data{sep}outputs{sep}instruction_set.txt"
 
     # Loading API Key
     load_dotenv()
-    secret_key = os.getenv('OPENAI_API_KEY')
+    secret_key = os.getenv("OPENAI_API_KEY")
 
     # generate instructions
     print("Generating Instruction Set...")
-    instr_generator = TranscriptConversion(model="text-davinci-003", secret_key=secret_key)
+    instr_generator = TranscriptConversion(
+        model="text-davinci-003", secret_key=secret_key
+    )
     instr_set = instr_generator.generateInstructions(transcript_dir=input_dir)
     print("Done!\n")
 
@@ -30,7 +40,6 @@ if __name__ == "__main__":
     if instr_set != None:
         with open(output_dir, "w") as file:
             file.write(instr_set)
-        print(f'Saved to \"%s\"' % output_dir)
+        print(f'Saved to "%s"' % output_dir)
     else:
         print("Error: Instruction Set has not been generated")
-
